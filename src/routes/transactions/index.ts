36e95e38a9a55f598e7140015c635c510f53bacf
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-07-12 14:55:17 -0300

    feat: adding route to handle balance and using cookies

--- src/routes/transactions/index.ts
+++ src/routes/transactions/index.ts
@@ -8,7 +8,7 @@ import { FastifyInstance } from 'fastify'
 import { randomUUID } from 'crypto'
 
 export async function transactionRoutes(app: FastifyInstance) {
-  app.get('/', async (_, res) => {
+  app.get('/', async (req, res) => {
     const transactions = await knex('transactions').select()
 
     return res.status(200).send({ transactions })
@@ -24,13 +24,35 @@ export async function transactionRoutes(app: FastifyInstance) {
     return res.status(200).send({ transaction })
   })
 
+  app.get('/balance', async (_, res) => {
+    const balance = await knex('transactions')
+      .sum('amount', {
+        as: 'amount',
+      })
+      .first()
+
+    return res.status(200).send({ balance })
+  })
+
   app.post('/', async (req, res) => {
     const { amount, title, type } = createTransactionBodySchema.parse(req.body)
+
+    let { sessionId } = req.cookies
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      res.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      })
+    }
 
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     return res.status(201).send()
