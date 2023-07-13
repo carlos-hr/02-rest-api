50a48bdae1cd888869301f6eeb98db7078e3c1eb
carlos-hr <carlos.henrique17.chr@gmail.com>
2023-07-13 14:37:25 -0300

    feat: creating balance and get transaction by id test

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,7 +1,8 @@
 import req from 'supertest'
-import { it, afterAll, beforeAll, describe, expect } from 'vitest'
+import { it, afterAll, beforeAll, describe, expect, beforeEach } from 'vitest'
 
 import { app } from '../src/app'
+import { execSync } from 'node:child_process'
 
 describe('Transaction routes', () => {
   beforeAll(async () => {
@@ -10,6 +11,11 @@ describe('Transaction routes', () => {
 
   afterAll(async () => {
     await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex -- migrate:rollback --all')
+    execSync('npm run knex -- migrate:latest')
   })
 
   it('should be able to create a transaction', async () => {
@@ -23,7 +29,7 @@ describe('Transaction routes', () => {
       .expect(201)
   })
 
-  it.only('should be able to list all transactions', async () => {
+  it('should be able to list all transactions', async () => {
     const newTransacationResponse = await req(app.server)
       .post('/transactions')
       .send({
@@ -45,5 +51,61 @@ describe('Transaction routes', () => {
         amount: 1000,
       }),
     ])
+  })
+
+  it('should be able to get a transaction by it id', async () => {
+    const newTransacationResponse = await req(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Vitest request',
+        type: 'credit',
+        amount: 1000,
+      })
+
+    const cookies = newTransacationResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await req(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const { id } = listTransactionsResponse.body.transactions[0]
+
+    const transactionById = await req(app.server)
+      .get(`/transactions/${id}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(transactionById.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'Vitest request',
+        amount: 1000,
+      }),
+    )
+  })
+
+  it.only('should be able to get a account balance', async () => {
+    const newTransacationResponse = await req(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Vitest request',
+        type: 'credit',
+        amount: 10000,
+      })
+
+    const cookies = newTransacationResponse.get('Set-Cookie')
+
+    await req(app.server).post('/transactions').set('Cookie', cookies).send({
+      title: 'Vitest second request',
+      type: 'debit',
+      amount: 1000,
+    })
+
+    const balanceResponse = await req(app.server)
+      .get('/transactions/balance')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(balanceResponse.body.balance).toEqual({ amount: 9000 })
   })
 })
